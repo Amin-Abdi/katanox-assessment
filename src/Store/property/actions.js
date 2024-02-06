ea5f165d403537b73349221296508b7e1ad8f836
Amin Abdi <malmaaz78@gmail.com>
2024-02-06 22:04:41 +0000

    add update policies to service and actions

--- src/Store/property/actions.js
+++ src/Store/property/actions.js
@@ -5,3 +5,8 @@ export const getPropertiesResponse = createAction(`[PROPERTIES] GET_PROPERTIES_R
 
 export const getPolicies = createAction(`[POLICIES] GET_POLICIES`);
 export const getPoliciesResponse = createAction(`[POLICIES] GET_POLICIES_RESPONSE`);
+
+export const updatePolicies = createAction(`[POLICIES] UPDATE_POLICIES`, (updatedPolicies) => ({
+    payload: updatedPolicies
+}));
+export const updatePoliciesResponse = createAction(`[POLICIES] UPDATE_POLICIES_RESPONSE`);
--- src/Store/property/service.js
+++ src/Store/property/service.js
@@ -13,8 +13,24 @@ export const getPolicies = async () => {
   return fetch('./data.json')
       .then(() => {
         const policies = data.data.map((policy) => policy.policies);
+        // console.log('pol', policies);
         return policies})
       .catch((error) => []
     )
 }
+
+export const updatePolicies = async (updatedPolicies) => {
+  
+  const newData = JSON.parse(JSON.stringify(data.data));
+  const propertyIndex = newData.findIndex((property) => property.property.id === updatedPolicies.noShowPolicies[0].propertyId);
+
+  if (propertyIndex !== -1) {
+    newData[propertyIndex].policies.noShowPolicies = updatedPolicies.noShowPolicies;
+    newData[propertyIndex].policies.cancellationPolicies = updatedPolicies.cancellationPolicies;
+  }
+
+  data.data = newData;
+
+  return updatedPolicies;
+};
 
